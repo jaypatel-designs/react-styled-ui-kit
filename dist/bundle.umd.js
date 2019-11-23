14760f9845f8db32dc0234e27fba0841ccc08d3b
Jay Patel <jaypatel@Jays-MBP.home>
2019-11-22 22:46:01 -0500

    Added rollup and babel with react

---
+++ dist/bundle.umd.js
@@ -0,0 +1,8 @@
+(function (factory) {
+	typeof define === 'function' && define.amd ? define(factory) :
+	factory();
+}((function () { 'use strict';
+
+	console.log('This is' + ' Value ' + 'the Message');
+
+})));
